a71bf1fb13d17a8663f7588a8ca12c3cb14408fd
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-06-01 19:54:06 +0400

    add mainBtn and MainTitle components, finish the section of medical story

---
+++ src/components/MainBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="btn" :class="{'btn__withBg': props.withBg}">{{ props.btnContent }}</button>
+</template>
+
+<script setup>
+import defineProps from 'vue';
+
+const props = defineProps({
+  btnContent: String,
+  withBg: Boolean
+})
+</script>
+
+<style scoped lang="scss">
+.btn {
+  border: 2px solid #0dcaf0;
+  color: #0dcaf0;
+  padding: 10px 20px;
+  font-size: 16px;
+  margin-right: 20px;
+  cursor: pointer;
+  text-align: center;
+  background: none;
+  border-radius: 25px;
+
+  &:hover {
+    background: #0dcaf0;
+    color: #fff;
+    border: 2px solid #0dcaf0;
+  }
+
+  &__withBg {
+    background: #273053;
+    border: 2px solid #273053;
+    color: #fff;
+    outline: none;
+  }
+}
+</style>
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="title">{{ props.main }} <span class="title__important">{{ props.important }}</span></h1>
+</template>
+
+<script setup >
+import defineProps from 'vue';
+
+const props = defineProps({
+  main: String,
+  important: String
+})
+</script>
+
+<style scoped lang="scss">
+.title {
+    font-size: 48px;
+    color: #000;
+    font-family: sans-serif;
+    text-transform: capitalize;
+    font-weight: 700;
+
+  &__important {
+    text-decoration: underline;
+    color: #0dcaf0;
+  }
+}
+</style>
